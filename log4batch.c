c56494b086e02fa431fff07a4c7c13df9a186bde
Werner Suess <werner.suess@itsv.at>
2018-10-01 08:55:14 +0200

    debugs removed

--- log4batch.c
+++ log4batch.c
@@ -7,20 +7,23 @@ log4batch , C-Variante
 #include <unistd.h>
 #include <string.h>
 
+// Tme structure http://www.cplusplus.com/reference/ctime/tm/
 struct tm *tmnow;
 
 void today(void) {
-   time_t tnow;
-   time(&tnow);
-   tmnow = localtime(&tnow);
-   printf("%d-%02d-%02d %02d:%02d:%02d>",
-      tmnow->tm_year + 1900,
-      tmnow->tm_mon + 1,
-      tmnow->tm_mday,
-      tmnow->tm_hour,
-      tmnow->tm_min,
-      tmnow->tm_sec
-  );
+    // type time_t:
+    time_t tnow;
+    // function time():
+    time(&tnow);
+    tmnow = localtime(&tnow);
+    printf("%d-%02d-%02d %02d:%02d:%02d>",
+        tmnow->tm_year + 1900,
+        tmnow->tm_mon + 1,
+        tmnow->tm_mday,
+        tmnow->tm_hour,
+        tmnow->tm_min,
+        tmnow->tm_sec
+    );
 }
 
 int main (int argc, char *argv[])
@@ -34,29 +37,26 @@ int main (int argc, char *argv[])
         switch (opt)
         {
         case 'd':
-            if ( strcmp(argv[3] , debug ) == 0) {
-                printf ("DAS IST EINE DEBUG MESSAGE \n");
-            }
+            //if ( strcmp(argv[3] , debug ) == 0) {
+            //    printf ("DAS IST EINE DEBUG MESSAGE \n");
+            //}
             printf ("[%s]\t", optarg);
             today();
             
             for(i=3; i < argc; i++) {
-                //fixme ... =)
-                //stringvergleich um 
-                //if ( strcmp(argv[i], pstring) != 0 ) {
-                    printf(" %s", argv[i]);
-                //}
+                printf(" %s", argv[i]);
             }
             printf ("\n");
             return 0;
-            //break;
+
         case 'h':
             printf ("Usage:\n");
             printf (" log4batch -d INFO|ERROR|WARN|DEBUG message text\n");
             return 0;
-            //break;
+
         }    
     } //while
+    
     for(i=1; i < argc; i++) {
         printf("%s ", argv[i]);
     }
